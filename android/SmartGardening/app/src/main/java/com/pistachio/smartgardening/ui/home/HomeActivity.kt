5eb9798ca2ee1860dc077a11c41121b3ca113e7e
Raindy Hardiansyah <raindyhardian@gmail.com>
2021-05-04 12:25:38 +0700

    first commit

---
+++ android/SmartGardening/app/src/main/java/com/pistachio/smartgardening/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.pistachio.smartgardening.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pistachio.smartgardening.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
