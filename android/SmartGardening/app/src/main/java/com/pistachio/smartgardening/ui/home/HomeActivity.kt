51f2bcdef7645f98772cc7fcecce1569934fb998
Raindy Hardiansyah <raindyhardian@gmail.com>
2021-05-10 11:35:56 +0700

    add home menu

--- android/SmartGardening/app/src/main/java/com/pistachio/smartgardening/ui/home/HomeActivity.kt
+++ android/SmartGardening/app/src/main/java/com/pistachio/smartgardening/ui/home/HomeActivity.kt
@@ -2,11 +2,15 @@ package com.pistachio.smartgardening.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.pistachio.smartgardening.R
+import com.pistachio.smartgardening.databinding.ActivityMainBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
