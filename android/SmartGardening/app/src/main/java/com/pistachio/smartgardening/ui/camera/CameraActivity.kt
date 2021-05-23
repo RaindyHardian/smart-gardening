5701698875f5aa383280be1a930e702df30d2836
Raindy Hardiansyah <raindyhardian@gmail.com>
2021-05-23 17:11:50 +0700

    add tensorflow lite model

--- android/SmartGardening/app/src/main/java/com/pistachio/smartgardening/ui/camera/CameraActivity.kt
+++ android/SmartGardening/app/src/main/java/com/pistachio/smartgardening/ui/camera/CameraActivity.kt
@@ -2,6 +2,7 @@ package com.pistachio.smartgardening.ui.camera
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -25,8 +26,23 @@ import androidx.core.content.ContextCompat
 import com.pistachio.smartgardening.R
 import com.pistachio.smartgardening.databinding.ActivityCameraBinding
 import kotlinx.android.synthetic.main.activity_camera.*
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.common.TensorOperator
+import org.tensorflow.lite.support.common.TensorProcessor
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp
+import org.tensorflow.lite.support.label.TensorLabel
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.io.File
+import java.io.FileInputStream
 import java.io.IOException
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
@@ -39,6 +55,19 @@ class CameraActivity : AppCompatActivity() {
 
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
+
+    protected var tflite: Interpreter? = null
+    private var inputImageBuffer: TensorImage? = null
+    private var imageSizeX = 0
+    private var imageSizeY = 0
+    private var outputProbabilityBuffer: TensorBuffer? = null
+    private var probabilityProcessor: TensorProcessor? = null
+    private val IMAGE_MEAN = 0.0f
+    private val IMAGE_STD = 1.0f
+    private val PROBABILITY_MEAN = 0.0f
+    private val PROBABILITY_STD = 255.0f
+    private lateinit var bitmap: Bitmap
+    private var labels: List<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,6 +92,12 @@ class CameraActivity : AppCompatActivity() {
 
         cameraExecutor = Executors.newSingleThreadExecutor()
 
+        try {
+            tflite = Interpreter(loadmodelfile(this))
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
         binding.btnRecapture.setOnClickListener {
             img_saved.visibility = View.GONE
             viewFinder.visibility = View.VISIBLE
@@ -71,6 +106,37 @@ class CameraActivity : AppCompatActivity() {
             camera_capture_button.visibility = View.VISIBLE
 
             cameraExecutor = Executors.newSingleThreadExecutor()
+        }
+
+        binding.btnConfirm.setOnClickListener{
+            val imageTensorIndex = 0
+            val imageShape =
+                tflite!!.getInputTensor(imageTensorIndex).shape() // {1, height, width, 3}
+
+            imageSizeY = imageShape[1]
+            imageSizeX = imageShape[2]
+            val imageDataType: DataType = tflite!!.getInputTensor(imageTensorIndex).dataType()
+
+            val probabilityTensorIndex = 0
+            val probabilityShape =
+                tflite!!.getOutputTensor(probabilityTensorIndex).shape() // {1, NUM_CLASSES}
+
+            val probabilityDataType: DataType =
+                tflite!!.getOutputTensor(probabilityTensorIndex).dataType()
+
+            inputImageBuffer = TensorImage(imageDataType)
+            outputProbabilityBuffer =
+                TensorBuffer.createFixedSize(probabilityShape, probabilityDataType)
+            probabilityProcessor =
+                TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build()
+
+            inputImageBuffer = loadImage(bitmap)
+
+            tflite!!.run(
+                inputImageBuffer?.getBuffer(),
+                outputProbabilityBuffer!!.getBuffer().rewind()
+            )
+            showresult()
         }
     }
 
@@ -105,13 +171,13 @@ class CameraActivity : AppCompatActivity() {
                     Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
                     Log.d(TAG, msg)
 
-                    var bitmap: Bitmap = BitmapFactory.decodeFile(photoFile.absolutePath)
+                    var oriBitmap: Bitmap = BitmapFactory.decodeFile(photoFile.absolutePath)
 
 //                    val rotateImage = getCameraPhotoOrientation(photoFile.absolutePath)
 //                    binding.imgSaved.rotation = rotateImage.toFloat()
 
-                    val rotateBitmap = rotateImageIfRequired(bitmap, savedUri)
-                    binding.imgSaved.setImageBitmap(rotateBitmap)
+                    bitmap = rotateImageIfRequired(oriBitmap, savedUri)
+                    binding.imgSaved.setImageBitmap(bitmap)
 
                     binding.imgSaved.visibility = View.VISIBLE
                     binding.viewFinder.visibility = View.GONE
@@ -230,7 +296,7 @@ class CameraActivity : AppCompatActivity() {
     }
 
     @Throws(IOException::class)
-    private fun rotateImageIfRequired(img: Bitmap, selectedImage: Uri): Bitmap? {
+    private fun rotateImageIfRequired(img: Bitmap, selectedImage: Uri): Bitmap {
         val ei = ExifInterface(selectedImage.path!!)
         val orientation =
             ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
@@ -242,7 +308,7 @@ class CameraActivity : AppCompatActivity() {
         }
     }
 
-    private fun rotateImage(img: Bitmap, degree: Int): Bitmap? {
+    private fun rotateImage(img: Bitmap, degree: Int): Bitmap {
         val matrix = Matrix()
         matrix.postRotate(degree.toFloat())
         val rotatedImg = Bitmap.createBitmap(img, 0, 0, img.width, img.height, matrix, true)
@@ -253,5 +319,61 @@ class CameraActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return super.onSupportNavigateUp()
+    }
+
+    // tensor
+    private fun loadImage(bitmap: Bitmap): TensorImage? {
+        // Loads bitmap into a TensorImage.
+        inputImageBuffer!!.load(bitmap)
+
+        // Creates processor for the TensorImage.
+        val cropSize = Math.min(bitmap.width, bitmap.height)
+        // TODO(b/143564309): Fuse ops inside ImageProcessor.
+        val imageProcessor: ImageProcessor = ImageProcessor.Builder()
+            .add(ResizeWithCropOrPadOp(cropSize, cropSize))
+            .add(ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+            .add(getPreprocessNormalizeOp())
+            .build()
+        return imageProcessor.process(inputImageBuffer)
+    }
+
+    @Throws(IOException::class)
+    private fun loadmodelfile(activity: Activity): MappedByteBuffer {
+        val fileDescriptor = activity.assets.openFd("PlantModel.tflite")
+        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+        val fileChannel = inputStream.channel
+        val startoffset = fileDescriptor.startOffset
+        val declaredLength = fileDescriptor.declaredLength
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startoffset, declaredLength)
+    }
+
+    private fun getPreprocessNormalizeOp(): TensorOperator {
+        return NormalizeOp(IMAGE_MEAN, IMAGE_STD)
+    }
+
+    private fun getPostprocessNormalizeOp(): TensorOperator {
+        return NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD)
+    }
+
+    private fun showresult() {
+        try {
+            labels = FileUtil.loadLabels(this, "PlantModel.txt")
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+        val labeledProbability =
+            TensorLabel(labels!!, probabilityProcessor!!.process(outputProbabilityBuffer))
+                .mapWithFloatValue
+        val maxValueInMap = Collections.max(labeledProbability.values)
+        for ((key, value) in labeledProbability) {
+            if (value == maxValueInMap) {
+                Toast.makeText(
+                    this,
+                    "Object: ${key}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+        Log.d("PROB", labeledProbability.toString())
     }
 }
