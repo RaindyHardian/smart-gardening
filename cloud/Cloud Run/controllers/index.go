2a2c60f5f411544de5fb5ac171aa0a7bd767c4c5
Sultan Achmad Qum Masykuro NS <sultangendut@gmail.com>
2021-06-02 17:43:28 +0700

    fix(cloud_storage): fix return upload storage (#6)
    wrong remove code at merge conclict code at #5

--- cloud/Cloud Run/controllers/index.go
+++ cloud/Cloud Run/controllers/index.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func IndexGet(c *gin.Context) {
+func Index(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Bangkit Capstone B21-CAP0251",
 	})
