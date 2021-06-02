3554f3ae2d666efadbb602a8b1b6cc01e2afb497
Sultan Achmad Qum Masykuro NS <sultangendut@gmail.com>
2021-06-03 06:40:14 +0700

    fix(storage): change naming file (#7)
    move open image to every process

--- cloud/Cloud Run/services/storage/storage.go
+++ cloud/Cloud Run/services/storage/storage.go
@@ -7,10 +7,11 @@ import (
 	"mime/multipart"
 	"time"
 
+	"bangkit.academy/smartgardening/cloudrun/util"
 	"cloud.google.com/go/storage"
 )
 
-func Upload(image multipart.File, object string, bucket string) (string, error) {
+func Upload(image multipart.FileHeader, object string, bucket string, contentType string) (string, error) {
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 	if err != nil {
@@ -21,8 +22,14 @@ func Upload(image multipart.File, object string, bucket string) (string, error) 
 	ctx, cancel := context.WithTimeout(ctx, time.Second*60)
 	defer cancel()
 
+	img, err := util.OpenImage(image)
+	if err != nil {
+		return "", fmt.Errorf("failed open image storage: %v", err)
+	}
+	defer img.Close()
+
 	wc := client.Bucket(bucket).Object(object).NewWriter(ctx)
-	if _, err = io.Copy(wc, image); err != nil {
+	if _, err = io.Copy(wc, img); err != nil {
 		return "", fmt.Errorf("failed copy: %v", err)
 	}
 	if err := wc.Close(); err != nil {
